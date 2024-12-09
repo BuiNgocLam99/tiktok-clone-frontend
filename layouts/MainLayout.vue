54590268732d1118801c1506c9f32d7010a44d01
BuiNgocLam99 <lambn957@gmail.com>
2024-12-09 23:27:25 +0700

    header

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
         <TopNav />
-
-        <div>
-            
+        <div
+            :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''"
+            class="flex justify-between mx-auto w-full lg:px-2.5 px-0"
+        >
+            <div>
+                <SizeNavMain />
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-
+    const route = useRoute();
 </script>
